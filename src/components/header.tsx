b9e651d5ca0df9b778e6eda7e58924c52fed826e
dungtm-mc <dungtm@missingcorner.com>
2024-12-02 17:09:44 +0700

    feat: read csv file

---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <div className="mt-6 w-full">
+      <h1 className="mx-0 text-lg font-bold">Dependencies Graph Viewer</h1>
+    </div>
+  );
+};
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}', // Add your project file extensions here
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
