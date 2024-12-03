fd237b50edfe9aa20f21e83a728fa2424fd8493f
dungtm-mc <dungtm@missingcorner.com>
2024-12-03 11:09:32 +0700

    feat: map from csv data to obj data

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,18 @@ import { Header } from './components/header';
 import { useState } from 'react';
 import Papa from 'papaparse';
 
-function App() {
-  const [file, setFile] = useState<File | null>(null);
-  const [data, setData] = useState<string[][]>([]);
+type ObjectValue = {
+  type: string;
+  targetIds: string[];
+};
 
-  const handleChangeFile = () => {
+type Objects = Record<string, ObjectValue>;
+
+function App() {
+  const [data, setData] = useState<string[][]>([]);
+  const [objs, setObjs] = useState<Objects>({});
+
+  const handleChangeFile = (file: File | null) => {
     if (!file) return;
 
     Papa.parse(file, {
@@ -22,16 +29,44 @@ function App() {
     });
   };
 
+  const handleShowGraph = () => {
+    setObjs((prevObjs) => {
+      const newObjs = { ...prevObjs };
+
+      data.forEach((row, index) => {
+        if (index === 0) return; // Bỏ qua hàng tiêu đề
+
+        const [key1, type1, targetId, type2] = row;
+
+        if (!newObjs[key1]) {
+          newObjs[key1] = { type: type1, targetIds: [] };
+        }
+        if (!newObjs[key1].targetIds.includes(targetId)) {
+          newObjs[key1].targetIds.push(targetId);
+        }
+
+        if (!newObjs[targetId]) {
+          newObjs[targetId] = { type: type2, targetIds: [] };
+        }
+      });
+
+      console.log(newObjs);
+
+      return newObjs;
+    });
+  };
+
   return (
     <AppShell className="mt-8" header={<Header />}>
       <Flex gap={8} align="end">
         <FileInput
           accept=".csv"
-          onChange={setFile}
+          onChange={(file) => handleChangeFile(file)}
           label="Import file"
+          // @ts-ignore
           placeholder="Pick file"
         />
-        <Button onClick={handleChangeFile} disabled={!file}>
+        <Button disabled={!data} onClick={handleShowGraph}>
           Show Graph Viewer
         </Button>
       </Flex>
